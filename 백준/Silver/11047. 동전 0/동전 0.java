6fc02c8dbdded68bd027f71594a459b2de2c57c7
player7571 <154997788+player7571@users.noreply.github.com>
2024-01-13 00:34:48 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,31 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-
+//2024-01-12
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N= Integer.parseInt(st.nextToken());
         int K= Integer.parseInt(st.nextToken());
-        ArrayList<Integer> coin = new ArrayList<>();
+        int [] coin = new int[N];
         for(int i=0; i<N; i++) {
-            int num= Integer.parseInt(br.readLine());
-            if(num>K) continue;
-            coin.add(num);
+            coin[i]= Integer.parseInt(br.readLine());
         }
         int min=0;
-        int size = coin.size()-1;
-        while(K!=0){
-            if(K>=coin.get(size)) {
-                K-=coin.get(size);
-                min++;
+        for(int i=N-1; i>=0; i--){
+            if(K>=coin[i]){
+                min+=(K/coin[i]);
+                K-=(coin[i]*(K/coin[i]));
+                if(K==0) break;
             }
-            else size--;
         }
         System.out.print(min);
-
     }
 }
